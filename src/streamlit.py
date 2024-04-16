6856ee0c7fb44ceac972a152717a56a84592f08d
ju-ong <young290@lguplus.co.kr>
2024-04-16 17:00:29 +0900

    init

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,48 +1,68 @@
 import streamlit as st  # all streamlit commands will be available through the "st" alias
-from folium import folium
 from streamlit_folium import st_folium
 
+import folium
+
 from utils import chatlib
+import json
 
 st.set_page_config(
     page_title="안붐빌지도?",
     page_icon="👀",
 )  # HTML title
 st.title(":orange[?] 안붐빌지도 👀")  # page title
+m = folium.Map(location=[37.572504, 126.999001], zoom_start=16, control_scale=True)
+folium.Marker(
+    [37.572504, 126.999001], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
 
-map = folium.Map(location=[37.572504, 126.999001], zoom_start=13, control_scale=True)
-
-st_data = st_folium(map, width=800)
+st_data = st_folium(m, width=725)
 
 if "chat_history" not in st.session_state:  # see if the chat history hasn't been created yet
     st.session_state.chat_history = []  # initialize the chat history
-st.sidebar.header("편하게 물어보세요 👀")  # page title
+
+st.sidebar.header("혼잡 수치")  # page title
+side_container0 = st.sidebar.container(border=True)
+
+side_container0.image('https://github.com/ju-ong/test-repo/assets/126046181/7b2e9c0c-1591-4279-984b-e6575983c022')
+st.sidebar.header("편하게 물어보는 공간! 👀")  # page title
+
+side_container1 = st.sidebar.container(border=True, height=500)
+
+side_container2 = st.sidebar.container()
+input_text = side_container2.chat_input("언제 어디를 가고 싶은지 물어봐요 ")  # display a chat input box
+
 
 # Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
 for message in st.session_state.chat_history:  # loop through the chat history
-    with st.sidebar.chat_message(
+    with side_container1.chat_message(
         message["role"]
     ):  # renders a chat line for the given role, containing everything in the with block
-        st.sidebar.markdown(message["text"])  # display the chat content
+        if message["role"] == "assistant":
+            side_container1.markdown(message["text"]["explain"])  # display the chat content
+        else:
+            side_container1.markdown(message["text"])
 
-input_text = st.sidebar.chat_input("언제 어디를 가고 싶은지 물어봐요 ")  # display a chat input box
+
 
 if input_text:  # run the code in this if block after the user submits a chat message
-    with st.sidebar.chat_message("user"):  # display a user chat message
-        st.sidebar.markdown(input_text)  # renders the user's latest message
+    with side_container1.chat_message("user"):  # display a user chat message
+        side_container1.markdown(input_text)  # renders the user's latest message
 
-    with st.sidebar.chat_message("assistant"):  # display a bot chat message
-        result = st.sidebar.write_stream(
-            chatlib.get_chat_streaming_response(
-                input_text=input_text,
-                chat_history=st.session_state.chat_history
-            )
-        )  # display bot's latest response
+    chat_response = chatlib.get_chat_streaming_response(
+        request=input_text,
+        chat_history=st.session_state.chat_history
+    )
+
+    json_response = json.loads(chat_response)
+
+    with side_container1.chat_message("assistant"):  # display a bot chat message
+        side_container1.markdown(json_response["explain"])  # display bot's latest response
 
     st.session_state.chat_history.append(
         {"role": "user", "text": input_text}
     )  # append the user's latest message to the chat history
 
     st.session_state.chat_history.append(
-        {"role": "assistant", "text": result}
+        {"role": "assistant", "text": chat_response}
     )  # append the bot's latest message to the chat history
--- src/utils/chatlib.py
+++ src/utils/chatlib.py
@@ -1,11 +1,21 @@
 import os
 from operator import itemgetter
 
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents import SearchClient
+from azure.search.documents._generated.models import VectorizedQuery
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import AzureChatOpenAI
-from prompt import vectorstore
+from openai.lib.azure import AzureOpenAI
+
+client = AzureOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT", "").strip(),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("OPENAI_API_VERSION")
+)
+
 
 def get_llm(**kwargs):
     load_dotenv()
@@ -27,39 +37,68 @@ def get_llm(**kwargs):
 
 def get_example():
     return """
-        "user request": 내일 오후 3시 압구정 로데오거리 사람 많아?
-        "result": 네, 내일 오후 3시 압구정로데오거리는 혼잡할 것으로 예상됩니다. 혼잡도는 "약간 붐빔" 수준이며, 약 18,000명에서 20,000명이 이 지역을 이용할 것으로 예상됩니다.
+        "user request": 이번주 토요일 오후 3시 압구정 로데오거리 사람 많아?
+        "result":  {"congestion level" : "혼잡", "place_name" : "압구정 로데오거리", "latitude" : "37.527660935102", "longitude" : "127.04070357316", "explain": "약속날인 이번주 토요일 오후 3시의 압구정 로데오거리의 혼잡도를 체크했더니
+`압구정 로데오거리`의 오후 3시의 혼잡도는 혼잡 단계로, 숨이 턱 막힐지도? 사람 조심하세요!"
+"}
     """
 
 
-def get_chat_streaming_response(input_text, chat_history):
+def get_json_format():
+    return """
+      {"congestion level" : "약간붐빔", "place_name" : "서울역", "latitude" : "37.123", "longitude" : "127.234", "explain": "내일 오후 3시 서울역은 혼잡할 것으로 예상돼요! 혼잡도는 "약간 붐빔" 수준이며, 약 18,000명에서 20,000명이 이 지역을 이용할 것으로 예상돼요."}
+    """
+
+
+def search_data_from_rag(request):
+    endpoint = os.getenv('AZURE_AI_SEARCH_ENDPOINT')
+    credential = AzureKeyCredential(os.getenv('AZURE_AI_SEARCH_API_KEY'))
+    index_name = os.getenv('AZURE_AI_SEARCH_INDEX')
+    api_version = os.getenv('AZURE_AI_SEARCH_API_VERSION')
+    embedding_model_name = "text-embedding-ada-002"
+
+    search_client = SearchClient(endpoint=endpoint, index_name=index_name, credential=credential,
+                                 api_version=api_version)
+
+    embedding = client.embeddings.create(input=request, model=embedding_model_name).data[0].embedding
+    vector_query = VectorizedQuery(vector=embedding, k_nearest_neighbors=20, fields="pl_tm_wkd_vector", exhaustive=True)
+
+    results = search_client.search(
+        search_text=None,
+        vector_queries=[vector_query],
+        select=["place_name", "time", "cong_lvl", "minpp", "maxpp"],
+    )
+
+    return results
+
+
+def get_chat_streaming_response(request, chat_history):
     prompt = get_prompt()
 
     chain = (
-        {
-            "grounding": itemgetter("request")
-            | vectorstore.as_retriever(
-                search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.55, "k": 12}
-            )
-            | get_page_from_doc,
-            "request": itemgetter("request"),
-            "examples": itemgetter("examples"),
-            "chat_history": itemgetter("chat_history"),
-        }
-        | prompt
-        | capture_and_return
-        | get_llm()
-        | StrOutputParser()
+            {
+                "grounding": itemgetter("grounding"),
+                "request": itemgetter("request"),
+                "examples": itemgetter("examples"),
+                "chat_history": itemgetter("chat_history"),
+                "json_format": itemgetter("json_format")
+            }
+            | prompt
+            | capture_and_return
+            | get_llm()
+            | StrOutputParser()
     )
 
     input_prompt = {
-        "request": input_text,
+        "request": request,
+        "grounding": json_to_string(search_data_from_rag(request)),
         "examples": get_example(),
         "chat_history": chat_history,
+        "json_format": get_json_format()
     }
-
-    for chunks in chain.stream(input_prompt):
-        yield chunks
+    result = chain.invoke(input_prompt)
+    print(result)
+    return result
 
 
 def capture_and_return(data):
@@ -67,12 +106,17 @@ def capture_and_return(data):
     return data
 
 
-def get_page_from_doc(data):
-    print(f"len data {data}")
-    pages = map(lambda doc: doc.page_content, data)
-    rags = "\n".join(pages)
-    print(f"rag {rags}")
-    return rags
+def json_to_string(results):
+    grounding = ""
+    for result in results:
+        grounding = grounding + f"""
+        장소: {result['place_name']}
+        시간: {result['time']} 
+        최대 인원수: {result['maxpp']}
+        최소 인원수: {result['minpp']}
+        혼잡도: {result['cong_lvl']}
+        """
+    return grounding
 
 
 def get_prompt():
@@ -81,9 +125,16 @@ def get_prompt():
         You are a chatbot that predicts and informs you of congestion in a specific area on a specific date. 
         Please predict future congestion based on the data provided. Answer in Korean.
 
+        - 
+
+        - Generate output as the following json format. 
+            {json_format}
+
+        - There are four levels of congestion ["여유", "보통", "약간 붐빔", "혼잡"]
+
         ## grounding:
             {grounding}
-            
+
         ## examples: 
             {examples}
 
@@ -92,6 +143,10 @@ def get_prompt():
 
         ## chat:
             "user request": "{request}"
-            "result":
+            "result": 
         """
     )
+
+
+if __name__ == '__main__':
+    get_chat_streaming_response("내일 서울역 혼잡도 알려줘", "")
